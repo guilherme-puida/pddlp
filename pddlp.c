a2f74dc4ef80a8cf7c5c4d508c7822fc6c79edd6
Guilherme Puida <guilherme@puida.xyz>
2023-11-02 02:05:20 -0300

    tokenizer: tokenize 'not'

--- pddlp.c
+++ pddlp.c
@@ -50,6 +50,7 @@ const char *pddlp_token_type_names[] = {
 
 	[PDDLP_TOKEN_DECREASE] = "PDDLP_TOKEN_DECREASE",
 	[PDDLP_TOKEN_FORALL] = "PDDLP_TOKEN_FORALL",
+	[PDDLP_TOKEN_NOT] = "PDDLP_TOKEN_NOT",
 
 	[PDDLP_TOKEN_EOF] = "PDDLP_TOKEN_EOF",
 	[PDDLP_TOKEN_ERROR] = "PDDLP_TOKEN_ERROR",
@@ -204,7 +205,7 @@ name_type(struct pddlp_tokenizer *t)
 	// is-violated
 	// maximize
 	// minimize
-	// not
+	// OK not
 	// object
 	// or
 	// over
@@ -220,6 +221,7 @@ name_type(struct pddlp_tokenizer *t)
 	switch (t->start[0]) {
 	case 'd': return check_name(t, 1, 7, "ecrease", PDDLP_TOKEN_DECREASE);
 	case 'f': return check_name(t, 1, 5, "orall", PDDLP_TOKEN_FORALL);
+	case 'n': return check_name(t, 1, 2, "ot", PDDLP_TOKEN_NOT);
 	}
 	return PDDLP_TOKEN_NAME;
 }
