924d7755cdfcb8026da039313f83899d729b0229
Guilherme Puida <guilherme@puida.xyz>
2023-11-01 23:35:37 -0300

    tokenizer: tokenizer double-character tokens

--- pddlp.c
+++ pddlp.c
@@ -52,6 +52,16 @@ peek(struct pddlp_tokenizer *t)
 	return *t->current;
 }
 
+static int
+match(struct pddlp_tokenizer *t, char expected)
+{
+	if (is_at_end(t) || peek(t) != expected)
+		return 0;
+
+	advance(t);
+	return 1;
+}
+
 static void
 skip_whitespace(struct pddlp_tokenizer *t)
 {
@@ -83,6 +93,15 @@ make_token(struct pddlp_tokenizer *t, enum pddlp_token_type token_type)
 	token.column = t->column;
 
 	return token;
+}
+
+static struct pddlp_token
+make_if_match(struct pddlp_tokenizer *t,
+	      char expected,
+	      enum pddlp_token_type if_match,
+	      enum pddlp_token_type if_not_match)
+{
+	return make_token(t, match(t, expected) ? if_match : if_not_match);
 }
 
 static struct pddlp_token
@@ -126,6 +145,8 @@ pddlp_scan_token(struct pddlp_tokenizer *t)
 	case '-': return make_token(t, PDDLP_TOKEN_MINUS);
 	case '*': return make_token(t, PDDLP_TOKEN_STAR);
 	case '/': return make_token(t, PDDLP_TOKEN_SLASH);
+	case '<': return make_if_match(t, '=', PDDLP_TOKEN_LTE, PDDLP_TOKEN_LT);
+	case '>': return make_if_match(t, '=', PDDLP_TOKEN_GTE, PDDLP_TOKEN_GT);
 	}
 
 	return error_token(t, "unrecognized character");
