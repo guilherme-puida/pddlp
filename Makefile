7d3b744dcf1d22c52b257b19d1f2007e9d62ae70
Guilherme Puida <guilherme@puida.xyz>
2023-11-01 23:23:18 -0300

    all: basic test framework

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 CFLAGS = -Wall -Wextra -pedantic -std=c99
 
-.PHONY: all clean
+.PHONY: all clean test
 
 all: build/pddlp-tokenize
 
 clean:
 	rm -rf build
+
+test: build/pddlp-tokenize run-tests.tcl
+	./run-tests.tcl
 
 build/%: %.c pddlp.c pddlp.h
 	@mkdir -p $(@D)
---
+++ run-tests.tcl
@@ -0,0 +1,20 @@
+#!/usr/bin/env tclsh
+
+set tests [glob tests/tokenizer/*.test]
+
+foreach test $tests {
+  set actual [exec build/pddlp-tokenize $test]
+  set expected [file rootname $test].expected
+
+  set expectedexists [file exists $expected]
+
+  if {$expectedexists} {
+    set status [catch {exec diff <<$actual $expected - -Z} result]
+
+    if {$status == 0} {
+      puts "= $test OK"
+    } else {
+      puts "= $test FAILED"
+    }
+  }
+}
